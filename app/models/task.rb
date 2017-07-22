acbaba44780d9b2aa81b3fe78373d72f079239d1
ismanuri <ismantesting@gmail.com>
2017-07-22 04:36:11 +0000

    update course index and task show

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :course
+  
+  validates :course_id, presence: true
+  validates :title, presence: true
+  
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/ 
 end
